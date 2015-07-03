ee22db2a38c127398561f0b161fb375729baf94a
Owain Jones <contact@odj.me>
2015-07-03 13:36:55 +0000

    possibly fixed raspivid stalls

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ OUTPUT_PORT = 5001
 # UDP_PORT = 43110  # UDP port on which to broadcast telemetry. -1 to disable.
 UDP_PORT = -1
 HEADER_EVERY = 10
-DATA_FILE = '/root/data/flight_{timestamp}.csv.gz'  # File in which to save
+DATA_FILE = '/data/flight_{timestamp}.csv.gz'  # File in which to save
                                               # telemetry. None to disable.
-VIDEO_FILE = '/root/data/flight_{timestamp}.h264'
+VIDEO_FILE = '/data/flight_{timestamp}.h264'
 RESET_WAIT_SECS = 9  # when we first access MW board, it resets. Wait this long
 CMD_WAIT_TIME = 0.01  # minimum time between serial commands.
 ITER_TIME = 0.8
